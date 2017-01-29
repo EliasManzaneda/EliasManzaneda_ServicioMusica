d7022fe191ba52f7f577c2520fc96d55553ffe30
Elías Manzaneda <Elas Manzaneda>
2017-01-29 18:25:58 +0100

    Ejercicio_8.2

---
+++ app/src/main/java/serviciomusica/example/org/servicio_musica/ServicioMusica.java
@@ -0,0 +1,44 @@
+package serviciomusica.example.org.servicio_musica;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class ServicioMusica extends Service {
+    MediaPlayer reproductor;
+    @Override
+    public void onCreate() {
+        Toast.makeText(this, "Servicio creado", Toast.LENGTH_SHORT).show();
+        reproductor = MediaPlayer.create(this, R.raw.audio);
+    }
+
+    /*
+    //8.2
+    @Override
+    public int onStartCommand(Intent intent, int flags, int idArranque) {
+        Toast.makeText(this,"Servicio arrancado "+ idArranque, Toast.LENGTH_SHORT).show();
+        reproductor.start();
+        return START_STICKY;
+    }
+    */
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this,"Servicio detenido", Toast.LENGTH_SHORT).show();
+        reproductor.stop();
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return null;
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        Toast.makeText(this, "Servicio arrancado " +
+                startId, Toast.LENGTH_SHORT).show();
+        reproductor.start();
+    }
+
+}
